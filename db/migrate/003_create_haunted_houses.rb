37dacad9e6e31cedaf03e1e93d548bdb60e8edd7
Derric Fragosa <fderric@gmail.com>
2020-04-18 05:59:26 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,9 +7,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.string :location 
       t.string :theme
       t.float :price 
-      t.boolean :whether_theyre_family_friendly_or_not
-      t.date :opening_date 
-      t.date :closing_date
+      t.boolean :family_friendly
+      t.datetime :opening_date 
+      t.datetime :closing_date
       t.text :long_description
     end
   end 

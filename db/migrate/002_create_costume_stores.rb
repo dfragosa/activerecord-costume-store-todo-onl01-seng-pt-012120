2f8b6568f598fb63df9f26e40fc4136e047dfd34
Derric Fragosa <fderric@gmail.com>
2020-04-18 05:34:14 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,7 +5,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
     create_table :costume_stores do |t|
       t.string :name 
       t.string :location 
-      t.string :costume_inventory
+      t.integer :costume_inventory
       t.integer :number_of_employees 
       t.boolean :whether_or_not_its_still_in_business 
       t.time :opening_time
